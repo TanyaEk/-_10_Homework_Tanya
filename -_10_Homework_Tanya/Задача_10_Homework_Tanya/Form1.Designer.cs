ec5c2da6e2aa4e9ff28c3c8fee3c260d807ba68b
Tanya Angelova Eksarova 10klas <160726621+TanyaEk@users.noreply.github.com>
2025-01-13 19:01:58 +0200

    so all finished
    finished it

---
+++ -_10_Homework_Tanya/Задача_10_Homework_Tanya/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Задача_10_Homework_Tanya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxCourses = new System.Windows.Forms.ComboBox();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(188, 70);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // comboBoxCourses
+            // 
+            this.comboBoxCourses.FormattingEnabled = true;
+            this.comboBoxCourses.Location = new System.Drawing.Point(162, 124);
+            this.comboBoxCourses.Name = "comboBoxCourses";
+            this.comboBoxCourses.Size = new System.Drawing.Size(149, 21);
+            this.comboBoxCourses.TabIndex = 1;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(349, 91);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 2;
+            this.btnAddStudent.Text = "Add";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(61, 179);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.Size = new System.Drawing.Size(352, 150);
+            this.dataGridViewStudents.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 412);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.comboBoxCourses);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxCourses;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+    }
+}
+
---
+++ -_10_Homework_Tanya/Задача_10_Homework_Tanya/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача_10_Homework_Tanya
+{
+    public partial class Form1 : Form
+    {
+        // the "//" things are just notes, i am entering them here to read everything i need to know or learn.
+        // Of course, you can read the notes.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Student> students = new List<Student>();
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+            // what is null?? well null means "no object", it simply means nothing to be there.
+            //making a message to pop up on the screen when the textBox and comboBox is empty.
+            if (textBoxName.Text == null || textBoxName.Text == "" || comboBoxCourses.SelectedItem == null) 
+            {
+                // a message will pop up when everything is empty
+                MessageBox.Show("Please enter a name and of course select the course", "Valid Error");
+                return;
+            }
+
+            //adding the student like a list
+            // SelectedItem: property to an object, the ComboBox attempts to make that object the currently selected one in the list.
+            Student student = new Student();
+            student.Name = textBoxName.Text;
+            student.Course = comboBoxCourses.SelectedItem.ToString();
+            students.Add(student);
+
+            dataGridViewStudents.DataSource = null;
+            dataGridViewStudents.DataSource = students;
+
+            //what is SelectedIndex: indicates the zero-based index of the currently selected item in the combo box list.
+            textBoxName.Clear();
+            comboBoxCourses.SelectedIndex = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This is for the comboBoxCourses
+            comboBoxCourses.Items.Add("Programming");
+            comboBoxCourses.Items.Add("Math");
+            comboBoxCourses.Items.Add("Physics");
+            comboBoxCourses.Items.Add("Chemistry");
+        }
+    }
+
+    public class Student 
+    {
+        // for the student
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+
+}
