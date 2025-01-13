7919fe95f9598d02fcdbde55c1d206faaed4d9c3
Administrator <Administrator@DESKTOP-O715OF7>
2025-01-13 11:14:04 +0200

    Add project files.

---
+++ Задача_10_Homework_Tanya/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача_10_Homework_Tanya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Student> students = new List<Student>();
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Student 
+    {
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+
+
+}
